efe9a26ef0402e7697dfe504d676c930d8bd118a
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-05-11 00:34:46 +0900

    refactor polygon

--- internal/infrastructure/polygon/constructor.go
+++ internal/infrastructure/polygon/constructor.go
@@ -1,29 +1,34 @@
 package polygon
 
 import (
-	"os"
-	"github.com/polygon-io/client-go/websocket/models"
+	"context"
+
+	"github.com/Goboolean/shared-packages/pkg/resolver"
 	polygonws "github.com/polygon-io/client-go/websocket"
 )
 
 
-var (
-	POLYGON_API_KEY = os.Getenv("POLYGON_API_KEY")
-
-	DEFAULT_BUFFER_SIZE = 1000
-)
+var DEFAULT_BUFFER_SIZE = 1000
 
 
 type Subscriber struct {
-	conn *polygonws.Client
+	*polygonws.Client
 
-	ch chan models.EquityAgg
+	r Receiver
+	ctx context.Context
+	cancel context.CancelFunc
 }
 
-func New() *Subscriber {
 
-	c, err := polygonws.New(polygonws.Config{
-		APIKey:    POLYGON_API_KEY,
+
+func New(c *resolver.Config, r Receiver) *Subscriber {
+
+	if err := c.ShouldPWExist(); err != nil {
+		panic(err)
+	}
+
+	conn, err := polygonws.New(polygonws.Config{
+		APIKey:    c.Password,
 		Feed:      polygonws.RealTime,
 		Market:    polygonws.Stocks,
 	})
@@ -32,15 +37,32 @@ func New() *Subscriber {
 		panic(err)
 	}
 
-	if err := c.Connect(); err != nil {
+	if err := conn.Connect(); err != nil {
 		panic(err)
 	}
-	
-	return &Subscriber{conn: c}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	instance := &Subscriber{
+		Client: conn,
+		r: r,
+		ctx: ctx,
+		cancel: cancel,
+	}
+
+	go RelayMessageToReceiver(instance)
+
+	return instance
 }
 
 
 
+func (s *Subscriber) Close() error {
+	if err := s.Close(); err != nil {
+		return err
+	}
 
+	s.cancel()
 
-
+	return nil
+}
---
+++ internal/infrastructure/polygon/receiver.go
@@ -0,0 +1,44 @@
+package polygon
+
+import (
+	"log"
+
+	polygonws "github.com/polygon-io/client-go/websocket"
+	"github.com/polygon-io/client-go/websocket/models"
+)
+
+
+type Receiver interface {
+	OnReceivePolygonStockAggs(models.EquityAgg) error
+}
+
+
+func (p *Subscriber) SubscribeStocksSecAggs(stock string) error {
+
+	if err := p.Subscribe(polygonws.StocksSecAggs, stock); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+
+func RelayMessageToReceiver(c *Subscriber) {
+	for {
+		select {
+		case <- c.ctx.Done():
+			return
+		case err := <-c.Error():
+			log.Fatal("error: ", err)
+			return
+		case out, more := <-c.Output():
+			if !more {
+				return
+			}
+
+			if err := c.r.OnReceivePolygonStockAggs(out.(models.EquityAgg)); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
