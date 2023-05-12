89e1af9399d4c5cc9512ab94eaf4d834bb028040
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-05-13 08:02:27 +0900

    create receiver adapter & port

---
+++ internal/adapter/receiver/constructor.go
@@ -0,0 +1,28 @@
+package receiver
+
+import (
+	"sync"
+
+	"github.com/Goboolean/stock-fetch-server/internal/domain/port/in"
+)
+
+
+
+type StockReceiveAdapter struct {
+	port in.RelayerPort
+}
+
+var (
+	instance *StockReceiveAdapter
+	once sync.Once
+)
+
+func New(port in.RelayerPort) *StockReceiveAdapter {
+	once.Do(func() {
+		instance = &StockReceiveAdapter{
+			port: port,
+		}
+	})	
+	
+	return instance
+}
