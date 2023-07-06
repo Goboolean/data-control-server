ed7904153a1265b74a26c89dc84bca264a8bfbf8
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-07-06 15:56:36 +0900

    create isMarketOn() on polygon, buycycle

---
+++ internal/infrastructure/buycycle/constructor_test.go
@@ -0,0 +1,98 @@
+package buycycle_test
+
+import (
+	"os"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/Goboolean/fetch-server/internal/infrastructure/buycycle"
+	"github.com/Goboolean/fetch-server/internal/util/withintime"
+	"github.com/Goboolean/shared/pkg/resolver"
+	"github.com/joho/godotenv"
+)
+
+
+
+var (
+	instance *buycycle.Subscriber
+)
+
+
+func SetupBuycycle() {
+	instance = buycycle.New(&resolver.ConfigMap{
+		"HOST": os.Getenv("BUYCYCLE_HOST"),
+		"PORT": os.Getenv("BUYCYCLE_PORT"),
+	}, receiver)
+}
+
+func TeardownBuycycle() {
+	instance.Close()
+}
+
+
+func TestMain(m *testing.M) {
+
+	if err := os.Chdir("../../../"); err != nil {
+		panic(err)
+	}
+
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	SetupBuycycle()
+	code := m.Run()
+	TeardownBuycycle()
+
+	os.Exit(code)
+}
+
+
+
+
+func Test_Constructor(t *testing.T) {
+
+	if flag := isMarketOn(); flag {
+		t.Skip()
+	}
+}
+
+
+var (
+	once sync.Once
+	withinDurationChecker *withintime.WithinDurationChecker
+	isMarketOnCache bool
+)
+
+// Struct withinDurationChecker is initialized with information of the Korea stock market.
+// Value isMarketOnCache is cached at the time of first call, therefore inconsistency beween tests may not occur.
+// U can get the value by calling IsMarketOn() function.
+func isMarketOn() bool {
+
+	once.Do(func() {
+		var err error
+
+		withinDurationChecker, err = withintime.New(&withintime.Option{
+			Location: "Asia/Seoul",
+			Inclusion: withintime.ConditionList{
+				time.Monday:    &withintime.Condition{StartTime: "09:00", EndTime:   "15:30",},
+				time.Tuesday:   &withintime.Condition{StartTime: "09:00", EndTime:   "15:30",},
+				time.Wednesday: &withintime.Condition{StartTime: "09:00", EndTime:   "15:30",},
+				time.Thursday:  &withintime.Condition{StartTime: "09:00", EndTime:   "15:30",},
+				time.Friday:    &withintime.Condition{StartTime: "09:00", EndTime:   "15:30",},
+			},
+			Exclusion: withintime.ConditionList{
+				// TODO: add holiday
+				},
+			}, nil)
+
+		if err != nil {
+			panic(err)
+		}
+
+		isMarketOnCache = withinDurationChecker.IsTimeNowWithinDuration()
+	})
+	
+	return isMarketOnCache
+}
---
+++ internal/infrastructure/polygon/constructor_test.go
@@ -0,0 +1,94 @@
+package polygon_test
+
+import (
+	"os"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/Goboolean/fetch-server/internal/infrastructure/polygon"
+	"github.com/Goboolean/fetch-server/internal/util/withintime"
+	"github.com/Goboolean/shared/pkg/resolver"
+	"github.com/joho/godotenv"
+)
+
+
+
+var (
+	instance *polygon.Subscriber
+	receiver *TestReceiver
+)
+
+
+func SetupPolygon() {
+	instance = polygon.New(&resolver.ConfigMap{
+		"KEY":  os.Getenv("POLYGON_API_KEY"),
+	}, receiver)
+}
+
+func TeardownPolygon() {
+	instance.Close()
+}
+
+
+func TestMain(m *testing.M) {
+
+	if err := os.Chdir("../../../"); err != nil {
+		panic(err)
+	}
+
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	SetupPolygon()
+	code := m.Run()
+	TeardownPolygon()
+
+	os.Exit(code)
+}
+
+
+func Test_Constructor(t *testing.T) {
+	if err := instance.Ping(); err != nil {
+		t.Errorf("Ping() = %v", err)
+	}
+}
+
+
+
+var (
+	once sync.Once
+	withinDurationChecker *withintime.WithinDurationChecker
+	isMarketOnCache bool
+)
+
+// Struct withinDurationChecker is initialized with information of the USA stock market.
+// Value isMarketOnCache is cached at the time of first call, therefore inconsistency beween tests may not occur.
+// U can get the value by calling IsMarketOn() function.
+func isMarketOn() bool {
+
+	once.Do(func() {
+		var err error
+
+		withinDurationChecker, err = withintime.New(&withintime.Option{
+			Location: "America/New_York",
+			Inclusion: withintime.ConditionList{
+				time.Monday:    &withintime.Condition{StartTime: "09:30", EndTime:   "16:00",},
+				time.Tuesday:   &withintime.Condition{StartTime: "09:30", EndTime:   "16:00",},
+				time.Wednesday: &withintime.Condition{StartTime: "09:30", EndTime:   "16:00",},
+				time.Thursday:  &withintime.Condition{StartTime: "09:30", EndTime:   "16:00",},
+				time.Friday:    &withintime.Condition{StartTime: "09:30", EndTime:   "16:00",},
+			},
+			Exclusion: withintime.ConditionList{
+				// TODO: add holiday
+				},
+			}, nil)
+
+		if err != nil {
+			panic(err)
+		}
+	})
+	
+	return isMarketOnCache
+}
