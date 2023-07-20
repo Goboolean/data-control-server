c1178a988cf5735a2afca3ddc65e2ba3486e8bb7
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-07-20 15:54:59 +0900

    create config class

--- internal/domain/service/config/query.go
+++ internal/domain/service/config/query.go
@@ -1,36 +1,99 @@
 package config
 
-import "github.com/Goboolean/fetch-server/internal/domain/entity"
+import (
+	"context"
 
-func (m *ConfigurationManager) SetStockRelayableTrue(stock string) error {
-	return m.relayer.FetchStock(stock)
+	"github.com/Goboolean/fetch-server/internal/domain/entity"
+	"github.com/Goboolean/fetch-server/internal/domain/errs"
+)
+
+func (m *ConfigurationManager) SetStockRelayableTrue(ctx context.Context, stockId string) error {
+	return m.relayer.FetchStock(ctx, stockId)
 }
 
-func (m *ConfigurationManager) SetStockRelayableFalse(stock string) error {
-	return m.relayer.StopFetchingStock(stock)
+func (m *ConfigurationManager) SetStockRelayableFalse(ctx context.Context, stockId string) error {
+	return m.relayer.StopFetchingStock(ctx, stockId)
 }
 
-func (m *ConfigurationManager) SetStockStoreableTrue(stock string) error {
-	return m.persistence.SubscribeRelayer(stock)
+func (m *ConfigurationManager) SetStockStoreableTrue(ctx context.Context, stockId string) error {
+	return m.persistence.SubscribeRelayer(ctx, stockId)
 }
 
-func (m *ConfigurationManager) SetStockStoreableFalse(stock string) error {
-	return m.persistence.UnsubscribeRelayer(stock)
+func (m *ConfigurationManager) SetStockStoreableFalse(ctx context.Context, stockId string) error {
+	return m.persistence.UnsubscribeRelayer(ctx, stockId)
 }
 
-func (m *ConfigurationManager) SetStockTransmittableTrue(stock string) error {
-	return m.transmitter.SubscribeRelayer(stock)
+func (m *ConfigurationManager) SetStockTransmittableTrue(ctx context.Context, stockId string) error {
+	return m.transmitter.SubscribeRelayer(ctx, stockId)
 }
 
-func (m *ConfigurationManager) SetStockTransmittableFalse(stock string) error {
-	return m.transmitter.SubscribeRelayer(stock)
+func (m *ConfigurationManager) SetStockTransmittableFalse(ctx context.Context, stockId string) error {
+	return m.transmitter.UnsubscribeRelayer(ctx, stockId)
 }
 
-func (m *ConfigurationManager) GetStockConfiguration(stock string) (entity.StockConfiguration, error) {
 
+func (m *ConfigurationManager) GetStockConfiguration(ctx context.Context, stock string) (entity.StockConfiguration, error) {
+
+	tx, err := m.tx.Transaction(context.Background())
+	defer tx.Rollback()
+	if err != nil {
+		return entity.StockConfiguration{}, err
+	}
+
+	exists, err := m.db.CheckStockExists(tx, stock)
+	if err != nil {
+		return entity.StockConfiguration{}, err
+	}
+
+	if !exists {
+		return entity.StockConfiguration{}, errs.StockNotFound
+	}
+	// check stock exist
+	// reflect stock info to entity
+
+
+	if err := tx.Commit(); err != nil {
+		return entity.StockConfiguration{}, err
+	}
+
+	return entity.StockConfiguration{}, nil
 }
 
-func (m *ConfigurationManager) GetAllStockConfiguration() ([]entity.StockConfiguration, error) {
- // get all stock list
- // reflect all stock info to list
+
+func (m *ConfigurationManager) GetAllStockConfiguration(ctx context.Context) ([]entity.StockConfiguration, error) {
+
+	tx, err := m.tx.Transaction(context.Background())
+	defer tx.Rollback()
+	if err != nil {
+		return nil, err
+	}
+
+	metaList, err := m.db.GetAllStockMetadata(tx)
+	if err != nil {
+		return nil, err
+	}
+
+	confList := make([]entity.StockConfiguration, 0)
+
+	for _, meta := range metaList {
+		stockId := meta.StockID
+		
+		if !m.relayer.IsStockRelayable(stockId) {
+			continue
+		}
+
+		isStoreable := m.persistence.IsStockStoreable(stockId)
+		isTransmittable := m.transmitter.IsStockTransmittable(stockId)
+
+		conf := entity.StockConfiguration{
+			StockId: stockId,
+			Relayable: true,
+			Storeable: isStoreable,
+			Transmittable: isTransmittable,
+		}
+
+		confList = append(confList, conf)
+	}
+
+	return confList, nil
 }
