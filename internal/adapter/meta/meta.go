090e41357c40279ad89a106a4d55cd518019aec8
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-07-23 01:46:10 +0900

    fix typo

--- internal/adapter/meta/meta.go
+++ internal/adapter/meta/meta.go
@@ -6,28 +6,29 @@ import (
 	"github.com/Goboolean/fetch-server/internal/adapter/transaction"
 	"github.com/Goboolean/fetch-server/internal/domain/entity"
 	"github.com/Goboolean/fetch-server/internal/domain/port"
+	"github.com/Goboolean/fetch-server/internal/domain/port/out"
 	"github.com/Goboolean/shared/pkg/rdbms"
 )
 
 
 
 type Adapter struct {
-	q *rdbms.Queries
+	rdbms *rdbms.Queries
 }
 
-func NewAdapter(q *rdbms.Queries) *Adapter {
-	return &Adapter{q: q}
+func NewAdapter(rdbms *rdbms.Queries) out.StockMetadataPort {
+	return &Adapter{rdbms: rdbms}
 }
 
 
 func (a *Adapter) CheckStockExists(tx port.Transactioner, stockId string) (bool, error) {
-	q := a.q.WithTx(tx.(*transaction.TxSession).R.Transaction().(*sql.Tx))
+	q := a.rdbms.WithTx(tx.(*transaction.TxSession).P.Transaction().(*sql.Tx))
 	return q.CheckStockExist(tx.Context(), stockId)
 }
 
 
 func (a *Adapter) GetStockMetadata(tx port.Transactioner, stockId string) (entity.StockAggsMeta, error) {
-	q := a.q.WithTx(tx.(*transaction.TxSession).R.Transaction().(*sql.Tx))
+	q := a.rdbms.WithTx(tx.(*transaction.TxSession).P.Transaction().(*sql.Tx))
 
 	dto, err := q.GetStockMeta(tx.Context(), stockId)
 
@@ -38,7 +39,7 @@ func (a *Adapter) GetStockMetadata(tx port.Transactioner, stockId string) (entit
 
 
 func (a *Adapter) GetAllStockMetadata(tx port.Transactioner) ([]entity.StockAggsMeta, error) {
-	q := a.q.WithTx(tx.(*transaction.TxSession).R.Transaction().(*sql.Tx))
+	q := a.rdbms.WithTx(tx.(*transaction.TxSession).P.Transaction().(*sql.Tx))
 
 	dtos, err := q.GetAllStockMetaList(tx.Context())
 	if err != nil {
