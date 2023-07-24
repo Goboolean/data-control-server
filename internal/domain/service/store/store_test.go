bbf399417bfdc11c5c4565db3db4ae4ac7c2970b
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-07-24 18:49:51 +0900

    create test code for store

---
+++ internal/domain/service/store/store_test.go
@@ -0,0 +1,45 @@
+package store_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Goboolean/fetch-server/internal/domain/service/store"
+)
+
+
+
+
+func Test_Store(t *testing.T) {
+
+	var stockId string = "test"
+
+	ctx := context.Background()
+
+	instance := store.New(ctx)
+
+	if exists := instance.StockExists(stockId); exists {
+		t.Errorf("stock exists before storing")
+		return
+	}
+
+	if err := instance.StoreStock(stockId); err != nil {
+		t.Errorf("failed to store stock: %v", err)
+		return
+	}
+
+	if exists := instance.StockExists(stockId); !exists {
+		t.Errorf("stock not exists after storing")
+		return
+	}
+
+	if err := instance.UnstoreStock(stockId); err != nil {
+		t.Errorf("failed to unstore stock: %v", err)
+		return
+	}
+
+	if exists := instance.StockExists(stockId); exists {
+		t.Errorf("stock exists before storing")
+		return
+	}	
+}
